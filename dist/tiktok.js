7d1662cee677d3e5da6155d57f41c0bfdbcd33c3
zhuowenli <zhuowenligg@gmail.com>
2022-03-06 15:56:37 +0800

    :art: 优化代码

--- dist/tiktok.js
+++ dist/tiktok.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TikTok成本设为4位小数
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       Zhuo Wenli
 // @match        https://ads.tiktok.com/*
